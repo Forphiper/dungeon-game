ab52cb01c01e718e5ba743c98a35621d67df2f15
Forphiper <cilin.joey@gmail.com>
2022-09-01 09:59:29 +0800

    Create a window and redering using SDL2

--- Makefile
+++ Makefile
@@ -1,24 +1,20 @@
 CC = g++
-CFLAGS = -Wall -g
+COMPILER_FLAGS = -Wall -g `sdl2-config --cflags`
 INCLUDES = -I /usr/include/SDL2
 RM = rm -f
-
+LINKER_FLAGS = `sdl2-config --libs` -L /usr/lib/x86_64-linux-gnu -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer
 OBJECTS = Animal.o Narwhal.o
 
 all: ${OBJECTS}
-	$(CC) ${CFLAGS} ${INCLUDES} main.cpp ${OBJECTS} -lSDL2main -lSDL2 -o main
-
-
-#all:
-#	$(CC) $(CFLAGS) main.cpp -o main
+	${CC} ${COMPILER_FLAGS} ${INCLUDES} main.cpp ${OBJECTS} ${LINKER_FLAGS} -o main
 
 Animal.o:Animal.cpp
-	$(CC) ${CFLAGS} Animal.cpp -c -o Animal.o
+	${CC} ${COMPILER_FLAGS} ${INCLUDES} Animal.cpp -c -o Animal.o
 
 Narwhal.o:Narwhal.cpp
-	$(CC) ${CFLAGS} Narwhal.cpp -c -o Narwhal.o
+	${CC} ${COMPILER_FLAGS} ${INCLUDES} Narwhal.cpp -c -o Narwhal.o
 
 
 clean:
-	$(RM) main
-	$(RM) $(OBJECTS)
+	${RM} main
+	${RM} ${OBJECTS}
--- main.cpp
+++ main.cpp
@@ -2,36 +2,93 @@
 #include <string>
 #include "Animal.h"
 #include "Narwhal.h"
-#include "SDL.h"
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
 
 using namespace std;
 
-int main() {
-    cout << "Hello World" << endl;
-
-    Animal a1("jeff", 48);
-    a1.eat();
-    a1.makeSound();
-    a1.display();
-
-    Narwhal n1("unicorn", 22, 2.1);
-    n1.eat();
-    n1.makeSound();
-    n1.swim();
-
-    Animal *aPtr = new Narwhal("kelp", 3, 0.5);
-    aPtr->eat();
-    aPtr->makeSound();
-    aPtr->display();
-
-    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO) != 0) {
-        SDL_Log("Unable to initialize SDL: %s\n", SDL_GetError());
+int main(int argc, char **argv) {
+    // Initialise SDL2
+    if(SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        // if init not return 0, then initialisation failed
+        cout << "SDL Init Error: " << SDL_GetError() << endl;
         return 1;
     }
 
-    std::cout << "Hello SDL2 with C++" <<std::endl;
+    // initialise subsystems we need e.g sdl image, mixer, ttf
+    // TODO: look up C++ bitwise logic
+    // image- being able to deal with other image types other than bmp. Also lets you play with surface pixel data
+    if((IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG) != IMG_INIT_PNG)
+    {
+        cout << "SDL Img Init Error: " << IMG_GetError() << endl;
+        return 1;
+    }
+    // ttf - text to font, for rendering text to screen
+    if(TTF_Init() != 0)
+    {
+        cout << "SDL TTF Init Error: " << TTF_GetError() << endl;
+        return 1;
+    }
+    // mixer - sound and music
+    if(Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096) == -1)
+    {
+        cout << "SDL Mixer Init Error: " << Mix_GetError() << endl;
+        return 1;
+    }
 
+    SDL_Window* window = SDL_CreateWindow("RPG GAME", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 320, 240, SDL_WINDOW_SHOWN);
+    if(window == nullptr)
+    {
+        cout << "SDL Window Error: " << SDL_GetError() << endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED  | SDL_RENDERER_PRESENTVSYNC);
+    if(renderer == nullptr)
+    {
+        cout << "SDL Renderer Error: " << SDL_GetError() << endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    bool keepLooping = true;
+    // Game Loop
+    while(keepLooping)
+    {
+        SDL_SetRenderDrawColor(renderer, 21, 209, 249, 255); // RGB (e.g R = 0-255)
+        // clear entire screen with current draw colour
+        SDL_RenderClear(renderer);
+
+        SDL_Rect rect;
+        rect.x = 10;
+        rect.y = 10;
+        rect.w = 50;
+        rect.h = 50;
+        SDL_SetRenderDrawColor(renderer, 34, 76, 22, 255);
+        // draws filled in rectangle to window using rectangles data
+        SDL_RenderFillRect(renderer, &rect);
+
+        // swaps drawing buffer
+        SDL_RenderPresent(renderer);
+
+        // ticks are milliseconds since the start of SDL init
+        if(SDL_GetTicks() > 5000) // 1000ms = 1second
+        {
+            keepLooping = false;
+        }
+    }
+
+    // Cleanup
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
     SDL_Quit();
+
+    cout << "end of code" << endl;
 
     return 0; 
 }
